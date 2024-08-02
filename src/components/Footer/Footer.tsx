ffa0a79fa4cb4330fc5dacc1c4f79be28e6dbd23
Marcel Hess <mhess@trask.cz>
2024-08-02 14:25:12 +0200

    router change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,22 +1,27 @@
-import { facebookIcon, instagramIcon } from "../../assets/icons"
-import styles from "./footer.module.css"
+import { facebookIcon, instagramIcon } from "../../assets/icons";
+import styles from "./footer.module.css";
 
 const Footer = () => {
+  const d = new Date();
+  const year = d.getFullYear();
   return (
     <footer id="footer" className={styles.footer}>
       <div className={styles.socials}>
-          <a className={styles.icon} href="https://www.facebook.com/photoartmk">
-            {facebookIcon}
-          </a>
-          <a className={styles.icon} href="https://www.instagram.com/miroslav_kravec">
-            {instagramIcon}
-          </a>
-        </div>
-        <small className={styles.copyright}>
-        &copy; 2024 | Czech Republic | Miroslav Kravec
+        <a className={styles.icon} href="https://www.facebook.com/photoartmk">
+          {facebookIcon}
+        </a>
+        <a
+          className={styles.icon}
+          href="https://www.instagram.com/miroslav_kravec"
+        >
+          {instagramIcon}
+        </a>
+      </div>
+      <small className={styles.copyright}>
+        &copy; {year} | Czech Republic | Miroslav Kravec
       </small>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.tsx";
+import Gallery from "./components/Gallery/Gallery.tsx";
+import Contact from "./pages/Contact/Contact.tsx";
+import Home from "./pages/Home/Home.tsx";
+import Portfolio from "./pages/Portfolio/Portfolio.tsx";
+import Pricelist from "./pages/Pricelist/Pricelist.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/cenik",
+        Component: Pricelist,
+      },
+      {
+        path: "/portfolio",
+        Component: Portfolio,
+      },
+      {
+        path: "/portfolio/:id",
+        Component: Gallery,
+      },
+      {
+        path: "/kontakt",
+        Component: Contact,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
